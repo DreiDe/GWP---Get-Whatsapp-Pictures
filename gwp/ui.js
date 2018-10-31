3b30eab8ada3c12e44ada6d723dd5a9b85b00260
DreiDe <44653977+DreiDe@users.noreply.github.com>
2018-10-31 23:45:53 +0100

    Add files via upload

--- gwp/ui.js
+++ gwp/ui.js
@@ -1,10 +1,10 @@
-let arr = [];//filled with [number, url, base64]
-let prefix = ["157", "178", "163", "177", "159", "176", "179", "151", "171", "160", "175", "170", "152", "173", "162", "174", "172"];//different number prefixes according to provider
-let numbers = [];//filed with randomly generated numbers
-let limit = 100;//number of numbers generated at one page load
+let arr = []; //filled with [number, url, base64]
+let prefix = ["157", "178", "163", "177", "159", "176", "179", "151", "171", "160", "175", "170", "152", "173", "162", "174", "172"]; //different number prefixes according to provider
+let numbers = []; //filed with randomly generated numbers
+let limit = 100; //number of numbers generated at one page load
 let count = 0;
 let index = 0;
-let countryCode = "49";//country code
+let countryCode = "49"; //country code
 
 const toDataURL = url => fetch(url)
     .then(response => response.blob())
@@ -13,39 +13,41 @@ const toDataURL = url => fetch(url)
         reader.onloadend = () => resolve(reader.result)
         reader.onerror = reject
         reader.readAsDataURL(blob)
-}))
+    }))
 
-function getpic(num){//get profile picture from number
-            let url = window.Store.Wap.profilePicFind(num + "@c.us");
+function getpic(num) { //get profile picture from number
+    let url = window.Store.Wap.profilePicFind(num + "@c.us");
 
-            function check1() {//check if url request is finished
-                if (url._value != undefined) {
-					index++;
-                    if(url._value.eurl != undefined){
-						count++;
-                    toDataURL(url._value.eurl).then(dataUrl => {
-						arr.push(num, url._value.eurl, dataUrl);
-                    })
-					}
-                } else {
-                    setTimeout(check1, 50);
-                }
+    function check1() { //check if url request is finished
+        if (url._value != undefined) {
+            index++;
+            if (url._value.eurl != undefined) {
+                count++;
+                toDataURL(url._value.eurl).then(dataUrl => {
+                    arr.push(num, url._value.eurl, dataUrl);
+                })
             }
-            check1();
+        } else {
+            setTimeout(check1, 50);
+        }
+    }
+    check1();
 }
-			
-						
+
+
 for (i = 0; i < limit; i++) {
-    numbers.unshift(countryCode + prefix[Math.floor(Math.random() * prefix.length)] + String(Math.floor(Math.random() * 100000000)));//generate random number
+    numbers.unshift(countryCode + prefix[Math.floor(Math.random() * prefix.length)] + String(Math.floor(Math.random() * 100000000))); //generate random number
 }
 
 
-function check() {//generate window.Store object after page load
+function check() { //generate window.Store object after page load
     if (document.getElementsByClassName('rAUz7')[0] != undefined) {
         window.Store = {};
-        webpackJsonp([], {"dgfhfgbdeb": (x, y, z) => window.Store.Wap = z('"dgfhfgbdeb"')}, "dgfhfgbdeb");
+        webpackJsonp([], {
+            "dgfhfgbdeb": (x, y, z) => window.Store.Wap = z('"dgfhfgbdeb"')
+        }, "dgfhfgbdeb");
         for (a = 0; a < numbers.length; a++) {
-		    getpic(numbers[a]);
+            getpic(numbers[a]);
         }
 
     } else {
@@ -54,16 +56,18 @@ function check() {//generate window.Store object after page load
 }
 check();
 
-function check2(){//check if all base64 strings exists
-	if(index == limit && count * 3 == arr.length){
-		if(arr.length != 0){
-		console.log(arr);
-		window.postMessage({ type: "FROM_PAGE", text: JSON.stringify(arr)}, "*");//send data to content.js			
-		}
-else{
-location = location;//reload page
-}
-	} else {
+function check2() { //check if all base64 strings exists
+    if (index == limit && count * 3 == arr.length) {
+        if (arr.length != 0) {
+            console.log(arr);
+            window.postMessage({
+                type: "FROM_PAGE",
+                text: JSON.stringify(arr)
+            }, "*"); //send data to content.js			
+        } else {
+            location = location; //reload page
+        }
+    } else {
         setTimeout(check2, 50);
     }
 }
--- gwp/whatsapp.php
+++ gwp/whatsapp.php
@@ -1,20 +1,24 @@
 <?php
-//$pdo = new PDO('mysql:host=localhost;dbname=whatsapp', '****', '******');
-//$pdo->exec("set names utf8");
-if(isset($_POST['user'])){
+// $pdo = new PDO('mysql:host=localhost;dbname=whatsapp', '****', '******');
+// $pdo->exec("set names utf8");
+
+if (isset($_POST['user']))
+	{
 	$arr = json_decode($_POST['user']);
 	define('UPLOAD_DIR', 'pics/');
-	for($i = 0; $i < count($arr); $i += 3){
-/* 	$statement = $pdo->prepare("Insert into user (number, url, base64) values (?, ?, ?)");
-	$statement->execute(array($arr[$i], $arr[$i + 1], $arr[$i + 2])); */
-	$img = $arr[$i + 2];
-	$img = str_replace('data:image/jpeg;base64,', '', $img);
-	$img = str_replace(' ', '+', $img);
-	$data = base64_decode($img);
-	$file = UPLOAD_DIR . $arr[$i] . date("d_m_Y__H_i_s"). '.jpg';//save image
-	file_put_contents($file, $data);
+	for ($i = 0; $i < count($arr); $i+= 3)
+		{
+		/* 	$statement = $pdo->prepare("Insert into user (number, url, base64) values (?, ?, ?)");
+		$statement->execute(array($arr[$i], $arr[$i + 1], $arr[$i + 2])); */
+		$img = $arr[$i + 2];
+		$img = str_replace('data:image/jpeg;base64,', '', $img);
+		$img = str_replace(' ', '+', $img);
+		$data = base64_decode($img);
+		$file = UPLOAD_DIR . $arr[$i] . date("d_m_Y__H_i_s") . '.jpg'; //save image
+		file_put_contents($file, $data);
+		}
 	}
-}
+
 echo "success";
-	exit();
+exit();
 ?>
